0a8365b98953de3db9a65020ab10a327fec3eddc
CodyGuo <guoxiao219@126.com>
2015-10-01 09:00:04 +0800

    skinh.dll只支持32位系统,优化例子

--- example/example.go
+++ example/example.go
@@ -5,48 +5,49 @@
 package main
 
 import (
-	"walk"
-	. "walk/declarative"
+    "github.com/lxn/walk"
+    . "github.com/lxn/walk/declarative"
 )
 import (
-	"log"
-	"skinh"
+    "github.com/codyguo/skinh"
+    "log"
 )
 
 func main() {
+    skinh.Attach()
+    skinh.SetAero(true)
+    skinh.SetWinMovable(1, true)
 
-	skinh.Attach()
+    var te *walk.TextEdit
 
-	var te *walk.TextEdit
-
-	if _, err := (MainWindow{
-		Title:   "Walk Clipboard Example",
-		MinSize: Size{300, 200},
-		Layout:  VBox{},
-		Children: []Widget{
-			PushButton{
-				Text: "Copy",
-				OnClicked: func() {
-					if err := walk.Clipboard().SetText(te.Text()); err != nil {
-						log.Print("Copy: ", err)
-					}
-				},
-			},
-			PushButton{
-				Text: "Paste",
-				OnClicked: func() {
-					if text, err := walk.Clipboard().Text(); err != nil {
-						log.Print("Paste: ", err)
-					} else {
-						te.SetText(text)
-					}
-				},
-			},
-			TextEdit{
-				AssignTo: &te,
-			},
-		},
-	}).Run(); err != nil {
-		log.Fatal(err)
-	}
+    if _, err := (MainWindow{
+        Title:   "Walk Clipboard Example",
+        MinSize: Size{300, 200},
+        Layout:  VBox{},
+        Children: []Widget{
+            PushButton{
+                Text: "Copy",
+                OnClicked: func() {
+                    if err := walk.Clipboard().SetText(te.Text()); err != nil {
+                        log.Print("Copy: ", err)
+                    }
+                },
+            },
+            PushButton{
+                Text: "Paste",
+                OnClicked: func() {
+                    if text, err := walk.Clipboard().Text(); err != nil {
+                        log.Print("Paste: ", err)
+                    } else {
+                        te.SetText(text)
+                    }
+                },
+            },
+            TextEdit{
+                AssignTo: &te,
+            },
+        },
+    }).Run(); err != nil {
+        log.Fatal(err)
+    }
 }
